41ff1e4d2f203cb75c8bdd73454ce5027df47d0e
TarekAbdo223 <tarekk.abdo22@gmail.com>
2025-11-29 15:48:25 +0200

    setup the colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const AppColors = {
+  primary: "#000",
+  white: "#fff",
+  black: "#000",
+  lightGray: "#f0f0f0",
+  background: "#f6f6f6",
+  disabledGray: "#d3d3d3",
+  blueGray: "#e6e8eA",
+  medGray: "#939393",
+};
