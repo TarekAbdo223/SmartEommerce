dfaf090a9bb6af979f604a9e00d01402f4dffb6f
TarekAbdo223 <tarekk.abdo22@gmail.com>
2026-01-07 16:43:09 +0200

    add the cart component

--- src/screens/cart/CartScreen.tsx
+++ src/screens/cart/CartScreen.tsx
@@ -1,18 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import HomeHeader from "../../components/headers/HomeHeader";
 import AppSaveView from "../../components/views/AppSaveView";
 import EmptyCart from "./EmptyCart";
 import CartItem from "../../components/cart/CartItem";
 import TotalsView from "../../components/cart/TotalsView";
+import { products } from "../../data/products";
+import { sharedPaddingHorizontal } from "../../styles/sharedStyles";
+import AppButton from "../../components/buttons/AppButton";
 
 const CartScreen = () => {
   return (
     <AppSaveView>
       <HomeHeader />
       {/* <EmptyCart /> */}
-      <CartItem />
-      <TotalsView />
+      <View style={{ paddingHorizontal: sharedPaddingHorizontal, flex: 1 }}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={products}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => {
+            return <CartItem {...item} />;
+          }}
+        />
+
+        <TotalsView itemsPrice={5000} orderTotal={5025} />
+        <AppButton title="Continue" />
+      </View>
     </AppSaveView>
   );
 };
